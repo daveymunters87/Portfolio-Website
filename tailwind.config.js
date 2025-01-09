1e4efdbbb104b740a2e6fc3cacb0a0c0574df29b
daveymunters87 <115430210+daveymunters87@users.noreply.github.com>
2025-01-09 09:16:55 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js,php}",
+    "./src/**/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#C43D3D',
+        secondary: '#F4DDDE',
+      },
+      fontFamily: {
+        banner: ['Banner', 'serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
